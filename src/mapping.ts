5c24e37369cefe198f1d94264b7723533c3924b0
Hancheng Zhou <z308114274@gmail.com>
2021-02-01 17:03:28 +0800

    🚀update contract

--- src/mapping.ts
+++ src/mapping.ts
@@ -53,11 +53,12 @@ export function handleCreateRedPacket(call: Create_red_packetCall): void {
   red_packet.rpid = rpid;
   red_packet.password = "PASSWORD INVALID"; // a password was stored locally and kept by creator
   red_packet.message = call.inputs._message;
+  red_packet.name = call.inputs._name;
   red_packet.hash = call.inputs._hash.toHexString();
   red_packet.total = call.inputs._total_tokens;
   red_packet.total_remaining = call.inputs._total_tokens;
   red_packet.duration = call.inputs._duration.toI32();
-  red_packet.shares = call.inputs._number;
+  red_packet.shares = call.inputs._number.toI32();
   red_packet.is_random = call.inputs._ifrandom;
   red_packet.creation_time = red_packet_info.creation_time;
   red_packet.last_updated_time = red_packet_info.creation_time;
@@ -108,6 +109,8 @@ export function handleCreationSuccess(event: CreationSuccess): void {
   // the creation of the pool happens when the call handler was triggered
   let red_packet_info = new RedPacketInfo(txHash);
   red_packet_info.rpid = event.params.id.toHexString();
+  red_packet_info.message = event.params.message;
+  red_packet_info.name = event.params.name;
   red_packet_info.creation_time = event.params.creation_time.toI32();
   red_packet_info.save();
 }
