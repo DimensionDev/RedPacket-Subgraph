59d66810fe456fdb1946032fdaa7f72cd87d28a3
Hancheng Zhou <z308114274@gmail.com>
2021-06-11 16:05:57 +0800

    🚀 deploy mumbai contract (#5)

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,7 @@
-export const CHAIN_ID = 3;
+export const CHAIN_ID = 80001;
+export { ERC20 } from "../generated/RedPacketV3/ERC20";
+export { ERC20NameBytes } from "../generated/RedPacketV3/ERC20NameBytes";
+export { ERC20SymbolBytes } from "../generated/RedPacketV3/ERC20SymbolBytes";
 
 export const ETH_ADDR = "0x0000000000000000000000000000000000000000";
 
